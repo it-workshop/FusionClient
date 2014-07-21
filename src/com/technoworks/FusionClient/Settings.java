6c9a5d85626c507fbd60162b3ca27520b7641727
lgor <forxgames007@gmail.com>
2014-07-22 00:47:45 +0400

    settings updated

--- src/com/technoworks/FusionClient/Settings.java
+++ src/com/technoworks/FusionClient/Settings.java
@@ -2,6 +2,11 @@ package com.technoworks.FusionClient;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
 
 /**
  * Created by lgor on 21.07.14.
@@ -13,4 +18,29 @@ public class Settings extends Activity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings);
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    public void saveProperties(View v){
+        EditText serverAddress = (EditText) findViewById(R.id.serverAddress);
+        EditText email = (EditText) findViewById(R.id.email);
+        CheckBox checkBox = (CheckBox) findViewById(R.id.checkShowMeOnMap);
+        Toast.makeText(this, "data saved, server : " + serverAddress.getText() + "\n email = " + email.getText() +
+                "\n show me = " + checkBox.isChecked(), Toast.LENGTH_SHORT).show();
+    }
+
+    public void validate(View v){
+        Log.d("mylog","validate");
+    }
+
+    @Override
+    public void onBackPressed() {
+        // if settings good, save and exit
+        super.onBackPressed();
+    }
+
+
 }
